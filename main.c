04600e4e05aa331e4b4853dfc1270547c41227f9
jerivas <jerivas@uaq.edu.mx>
2019-10-16 09:31:10 -0500

    Fixed major issues while compiling main.c Created .gitignore file

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(){
 	int a=0;
-	int b=0
+	int b=0;
 	int c=0;
-	c = a+b;
 
 	scanf("%d", &a);
 	scanf("%d", &b);
+	c = a+b;
+
 	printf("a+b = %d\n",c);
 
 	return 0;
